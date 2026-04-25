d2dc3692481154dc1c9ab68719cb6b04a2851f71
Norman Veliz <106999235+brzrker@users.noreply.github.com>
2026-04-25 15:07:24 -0400

    Corrige carga de estilos en producción agregando WhiteNoise

--- cerveceria_nacional/settings.py
+++ cerveceria_nacional/settings.py
@@ -63,6 +63,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'cerveceria_nacional.urls'
@@ -132,6 +133,8 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
